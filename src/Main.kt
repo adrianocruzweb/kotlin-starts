c198bad8ac885b6781241a1cfdfc79b18e628d1a
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-02-03 15:04:30 -0400

    fieat: collection list commit main

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+val systemUsers: MutableList<Int> = mutableListOf(1,2,3)
+val sudoers: List<Int> = systemUsers
+
+fun addSystemUsers(newUser: Int) {
+    systemUsers.add(newUser)
+}
+
+fun getSysSudoers(): List<Int> {
+    return sudoers
+}
+
+//val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3")
+
+
 fun main() {
-    val name = "Kotlin"
+    addSystemUsers(4)
+    println("Tot sudoers: ${getSysSudoers().size}")
+    getSysSudoers().forEach {
+        i -> println("Some useful info on user $i")
+    }
+
+    /*val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
@@ -10,5 +30,6 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
-    }
+    }*/
+
 }
