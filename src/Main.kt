cbf85eba67bd5c7d34a798f4b4e9f6000cb57473
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-02-19 07:40:07 -0400

    feat:transport parameter to superclass by inheritance constructor

--- src/Main.kt
+++ src/Main.kt
@@ -53,18 +53,29 @@ class Yorkshire: Dog(){
     }
 }*/
 
-open class Tiger(val origin: String) {
+/*open class Tiger(val origin: String) {
     fun sayHello() {
         println("A tiger from $origin says: grrhhh!")
     }
 }
 
-class SiberianTiger : Tiger("Siberia")
+class SiberianTiger : Tiger("Siberia")*/
+
+open class Lion(val name: String, val origin: String){
+    fun sayHello(){
+        println("$name, the lion from $origin says: graoh!")
+    }
+}
+
+class Asiatic(name: String) : Lion(name = name, origin = "India")
 
 fun main() {
 
-    val tiger: Tiger = SiberianTiger()
-    tiger.sayHello()
+    val lion: Lion = Asiatic("Rufo")
+    lion.sayHello()
+
+    /*val tiger: Tiger = SiberianTiger()
+    tiger.sayHello()*/
 
     /*val dog: Dog = Yorkshire()
     dog.sayHello()*/
